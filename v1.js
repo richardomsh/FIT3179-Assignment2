0ad53491c8f428b88b7f0d1f01d8de78bf7baa20
richardomsh <88014667+richardomsh@users.noreply.github.com>
2022-10-17 17:04:05 +0800

    Added assignemnt2 files

---
+++ v1.js
@@ -0,0 +1,29 @@
+var vg_1 = "v1.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "v2.json";
+vegaEmbed("#word", vg_2).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "v3.json";
+vegaEmbed("#hist", vg_3).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "v4.json";
+vegaEmbed("#box", vg_4).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "v5.json";
+vegaEmbed("#bar", vg_5).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
